b1193850c8039da21245fed5f3a7f91dd8d5ab0e
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-09-09 22:29:38 +0700

    finish

--- app/src/main/java/com/rujirakongsomran/kt_autocomplete/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_autocomplete/MainActivity.kt
@@ -2,10 +2,19 @@ package com.rujirakongsomran.kt_autocomplete
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val countries = resources.getStringArray(R.array.countries)
+        val arrayAdapter =
+            ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, countries)
+
+        autoCompleteTextView.setAdapter(arrayAdapter)
+
     }
 }
